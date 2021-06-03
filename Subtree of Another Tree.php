e864c71c2978a6bfd0e7e5d3a7ced7c654089dbf
andrewdcrypt <25671094+andrewdcrypt@users.noreply.github.com>
2021-06-03 04:43:03 -0700

    Add files via upload

---
+++ Subtree of Another Tree.php
@@ -0,0 +1,51 @@
+#Subtree of Another Tree
+
+Description: Check root tree if there is a exact duplicate of the sub tree that exist within it
+
+Method One:
+
+Set base condition
+Check if the root tree is the same as the subRoot tree
+Set the left and right variable recursion for it to loop 
+Check if $left or $right returns true to indicate a duplicate subRoot tree true exist within root true
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param TreeNode $root
+     * @param TreeNode $subRoot
+     * @return Boolean
+     */
+    function isSubtree($root, $subRoot) {
+        
+        if ($root === null){
+            return false;
+        }
+        
+        
+        if($root == $subRoot){
+            return true;
+        }
+        
+        
+        $left = $this->isSubtree($root->left, $subRoot);
+        $right = $this->isSubtree($root->right, $subRoot);
+        
+        if ($left || $right){
+            return true;
+        }
+        
+    }
+}
